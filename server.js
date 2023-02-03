9ec0c36d68050cfed6e72e453886837526e8b5a0
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2023-02-03 23:32:54 +0700

    create

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express =  require("express");
 const mydb = require('./config/db')
 const routeUser = require("./routes/userRouter")
 const app = express();
+const path = require('path');
 const port = process.env.port || 3002;
 app.use(express.json());
 
-
+//path to view
 app.set('view engine','ejs')
 app.set("views", "views")
 
-app.use(express.static('public'));
+//path to images,css
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images',express.static('images'));
+
+//path to page
 app.use("/user",routeUser);
 
 app.listen(port,()=> console.log('Server is running on port '+port));
