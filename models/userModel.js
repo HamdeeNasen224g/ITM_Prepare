0c1990dcc4a150fcddfcafc63bba29406b3e8c5b
wavezaza <80101494+wavezaza@users.noreply.github.com>
2023-02-03 23:15:13 +0700

    w

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,26 @@
 const db = require('../config/db')
 
-class Usermodel{
-    
-    static async getusers(){
+class Usermodel {
 
-        return new Promise(resolve =>{
-        db.query("select * from users",[],(error,results)=>{
-           if(!error){
-            resolve(results)
-           } 
+    static async getusers() {
+
+        return new Promise(resolve => {
+            db.query("select * from users", [], (error, results) => {
+                if (!error) {
+                    resolve(results)
+                }
+            })
         })
-    })
     }
 
-    static async insertImg(id,name,data){
-       
-        db.query("insert into image (name,id,data ) value (?,?,?)",[id,name,data],(err,result)=>{
-           if(!err)
-            resolve(true)
-           else
-           console.log("error",err)
-           resolve(false)
+    static async insertImg(id, name, data) {
+
+        db.query("insert into image (name,id,data ) value (?,?,?)", [id, name, data], (err, result) => {
+            if (!err)
+                resolve(true)
+            else
+                console.log("error", err)
+            resolve(false)
         })
     }
 
