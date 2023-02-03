ec354c1836e7f3454ebbd7a7165070ec3b8aa14f
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2023-02-03 21:06:52 +0700

    create user page

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,9 @@ const express = require('express');
 const UserController = require("../controllers/userController")
 const router = require('express').Router();
 
-router.get('/',(req,res)=>{
-    res.json({message:"Hello world"})
+router.get('/',(req,res,next)=>{
+    res.render('user');
+    // res.json({message:"Hello world"})
 }) 
 
 router.get('/alluser',UserController.getalluser)
