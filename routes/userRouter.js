1ae5173c03de846bbf89a574e58362f4c6fe434e
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2023-02-03 21:35:13 +0700

    edit

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,8 @@ const UserController = require("../controllers/userController")
 const router = require('express').Router();
 
 router.get('/',(req,res,next)=>{
-    res.render('user');
+     res.render('user');
+    // res.send('Hello World');
     // res.json({message:"Hello world"})
 }) 
 
