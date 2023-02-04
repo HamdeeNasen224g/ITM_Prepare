d10ad29c0bbf5ce91bb9e1fc4c2f05b0882dd92f
PaLo <84552476+PloyPalo@users.noreply.github.com>
2023-02-04 12:05:11 +0700

    dsd

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,23 +11,18 @@ router.get('/',(req,res,next)=>{
 router.get('/header', (req,res,next)=>{
     res.sendFile('layout/header.ejs');
 })
-
 router.get('/footer', (req,res,next)=>{
     res.sendFile('layout/footer.ejs');
 })
-
 router.get('/navbar', (req,res,next)=>{
     res.sendFile('layout/navbar.ejs');
 })
-
 router.get('/upload',(req,res,next)=>{
     res.render('img');
 }) 
-
 router.get('/add',(req,res,next)=>{ 
     res.render('add');
 })
-
 router.get('/show',(req,res,next)=>{ 
     res.render('show');
 })
